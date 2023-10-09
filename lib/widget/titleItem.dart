afd5293a21999dde72c2be661551e18dae23e153
BenjaminCCG <42145150+BenjaminCCG@users.noreply.github.com>
2023-10-09 17:52:29 +0800

    feat: submit

---
+++ lib/widget/titleItem.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TitleItem extends StatelessWidget {
+  const TitleItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 110,
+      height: 40,
+      child: Container(),
+    );
+  }
+}
